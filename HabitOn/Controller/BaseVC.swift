e9f26e11b4b9b3f11199bba1cb665973f7597fab
Danil <superkachenya@gmail.com>
2017-05-08 18:51:33 +0300

    add base classes; add pods;

---
+++ HabitOn/Controller/BaseVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HabitOn
+//
+//  Created by Danil on 08.05.17.
+//  Copyright © 2017 danielvoitenkoapps. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HabitOn/PersistenceContainer.swift
@@ -0,0 +1,9 @@
+//
+//  PersistenceContainer.swift
+//  HabitOn
+//
+//  Created by Danil on 08.05.17.
+//  Copyright © 2017 danielvoitenkoapps. All rights reserved.
+//
+
+import Foundation
